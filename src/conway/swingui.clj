d64d57343011dc5c24495b0e3a50d68133478e81
Mark Bastian <markbastian@gmail.com>
2015-03-24 19:59:38 -0600

    Moving from refs to atoms.

--- src/conway/swingui.clj
+++ src/conway/swingui.clj
@@ -1,13 +1,13 @@
 (ns conway.swingui
   (:gen-class)
-  (:require [conway.rules])
-  (:import (javax.swing JFrame JPanel JButton)
+  (:require [conway.rules :as rules])
+  (:import (javax.swing JFrame JPanel JButton BoxLayout Box)
            (java.awt BorderLayout Graphics2D Color ImageCapabilities)
            (java.awt.geom Rectangle2D$Double)
            (java.awt.event ActionListener)
            (java.awt.image VolatileImage)))
 
-(defn panel [grid-ref]
+(defn panel [grid]
   (let [pnl (proxy [JPanel] []
     (paint [g]
       (let [w (.getWidth this) 
@@ -18,44 +18,48 @@
         (doto g2d
           (.setPaint Color/BLACK)
           (.fill bg))
-        (doseq [i (range (count @grid-ref))]
-          (doseq [j (range (count (get @grid-ref i)))]
-            (let [c (if (= :alive (get-in @grid-ref [i j])) Color/GREEN Color/RED)
-                  sq (Rectangle2D$Double. (* i (/ w (count @grid-ref)))
-                                          (* j (/ h (count (get @grid-ref i))))
-                                          (/ w (count @grid-ref))
-                                          (/ h (count (get @grid-ref i))))]
+        (doseq [i (range (count @grid))]
+          (doseq [j (range (count (get @grid i)))]
+            (let [c (if (= :alive (get-in @grid [i j])) Color/GREEN Color/RED)
+                  sq (Rectangle2D$Double. (* i (/ w (count @grid)))
+                                          (* j (/ h (count (get @grid i))))
+                                          (/ w (count @grid))
+                                          (/ h (count (get @grid i))))]
               (do
                 (.setPaint g2d c)
                 (.fill g2d sq)))))
         (.drawImage g vimg 0 0 this)
         )))]
     (do 
-      (add-watch grid-ref :repaint (fn [_ _ _ _] (.repaint pnl)))
+      (add-watch grid :repaint (fn [_ _ _ _] (.repaint pnl)))
     pnl)))
 
-(defn sim [grid-ref]
+(defn sim [grid]
   (future (loop []
-    (dosync (ref-set grid-ref (conway.rules/step @grid-ref)))
-    (recur))))
+            (dosync (swap! grid rules/step))
+            (recur))))
 
-(defn button [grid-ref] 
-  (doto (JButton. "Go!")
-    (.addActionListener 
-      (reify ActionListener
-        (actionPerformed [_ _] 
-          (sim grid-ref)
-          ;(dosync (ref-set grid-ref (conway.rules/step @grid-ref)))
-          )))))
+(defn south-panel [grid-ref]
+  (doto (Box. BoxLayout/Y_AXIS)
+    (.add (doto (JButton. "Go")
+            (.addActionListener
+              (reify ActionListener
+                (actionPerformed [_ _] (sim grid-ref))))))
+    (.add (doto (JButton. "Reset")
+            (.addActionListener
+              (reify ActionListener
+                (actionPerformed [_ _] (reset! grid-ref (rules/seed-grid 100 100)))))))))
 
 (defn launch [exit-behavior]
-  (let [grid-ref (ref (conway.rules/seed-grid 100 100))]
+  (let [grid (atom (rules/seed-grid 100 100))]
     (doto (JFrame. "Conway's Game of Life")
       (.setSize 800 600)
       (.setDefaultCloseOperation exit-behavior)
-      (.add (panel grid-ref) BorderLayout/CENTER)
-      (.add (button grid-ref) BorderLayout/SOUTH)
+      (.add (panel grid) BorderLayout/CENTER)
+      (.add (south-panel grid) BorderLayout/SOUTH)
       (.setVisible true))))
 
 (defn -main [& args]
   (launch JFrame/EXIT_ON_CLOSE))
+
+(launch JFrame/DISPOSE_ON_CLOSE)
